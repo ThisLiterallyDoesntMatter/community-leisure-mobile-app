619f7028c773eb712048fe591cabb6fb26d8dc75
HugeBigAl <imhugebigal@gmail.com>
2017-03-18 22:13:55 +0000

    Add files via upload

---
+++ subject_list.js
@@ -0,0 +1,12 @@
+/**
+ * Created by B00291854 on 16/03/2017.
+ */
+
+//This data will allow us to seperate the subjects in a seperate JavaScript file
+
+var subjects = {
+    "Sports": "SPT",
+    "Meetup": "MTP",
+    "Events": "ENT",
+    "News": "NES"
+};
